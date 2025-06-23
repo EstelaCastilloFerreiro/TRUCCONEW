a4956e53e030eea0021685294a74e25b139e304a
estelaacastillo <estelacastilloferreiro@gmail.com>
2025-06-23 15:52:11 +0200

    Comment out background image to test app without it

--- app.py
+++ app.py
@@ -83,7 +83,7 @@ def set_background(image_file):
 # Login de seguridad
 if 'logueado' not in st.session_state:
     try:
-        set_background(get_asset_path("bg_truccoo.png"))  # Aplica fondo solo antes del login
+        # set_background(get_asset_path("bg_truccoo.png"))  # Aplica fondo solo antes del login
         with open(get_asset_path("Logo.png"), "rb") as f:
             st.image(f.read(), width=180)
         st.markdown("<div class='login-title'>Acceso a TRUCCO Analytics</div>", unsafe_allow_html=True)
